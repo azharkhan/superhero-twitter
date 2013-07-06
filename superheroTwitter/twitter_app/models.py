0a9b1edf7894dc331c5437aa4fac9a4e0ef73bf2
azharkhan <azmanian.devil@gmail.com>
2013-07-05 20:10:18 -0400

    adding initial models

--- superheroTwitter/twitter_app/models.py
+++ superheroTwitter/twitter_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Defining the Tweet model
+
+class Tweet(models.Model):
+  content = models.CharField(max_length=140)
+  user = models.ForeignKey(User)
+  date_created = models.DateTimeField(auto_now=True, blank=True)
+
+  def __unicode__(self):
+    return self.content
+
+# Profile Model - using django users model
+# maps users to their followers and people they follow
+# Symmetrical relationship turned off to allow one sided following
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User)
+  follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
